f95665198623d6c8fd50e1353fbaed344e82b61b
satyam <mail@satyamraval.com>
2016-10-21 17:44:30 +0530

    admin changes

---
+++ Source/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPortalMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Layout = "~/Views/Shared/Admin/_LayoutAdmin.cshtml"; 
+            return View();
+        }
+    }
+}
